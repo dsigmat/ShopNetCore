4ad28b3359b7054916601479ecfc4611dcdeb530
Denis Sidorov <dsigmat@hotmail.com>
2019-08-15 16:36:07 +0600

    создал два интерфейса

---
+++ ShopNetCore/Data/Interfaces/IAllCars.cs
@@ -0,0 +1,15 @@
+﻿using ShopNetCore.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopNetCore.Data.Interfaces
+{
+    interface IAllCars
+    {
+        IEnumerable<Car> Cars { get; set; }
+        IEnumerable<Car> GetFavCars { get; set; }
+        Car GetObjectCar(int carId);
+    }
+}
---
+++ ShopNetCore/Data/Interfaces/ICarsCategory.cs
@@ -0,0 +1,16 @@
+﻿using ShopNetCore.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopNetCore.Data.Interfaces
+{
+    interface ICarsCategory
+    {
+        //Возвращает список
+        IEnumerable<Category> AllCategories { get; }
+
+
+    }
+}
