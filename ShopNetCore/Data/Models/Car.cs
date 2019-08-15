10a7371417566b22a24a35c08babdcbd40fb2417
Denis Sidorov <dsigmat@hotmail.com>
2019-08-15 16:23:40 +0600

    создал две модели Cars end CAtegory

---
+++ ShopNetCore/Data/Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopNetCore.Data.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDesc { get; set; }
+        public string LongDesc { get; set; }
+        public string Img { get; set; }
+        public ushort Price { get; set; }
+        public bool IsFavorite { get; set; }
+        public int Available { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ ShopNetCore/Data/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopNetCore.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public int Desc { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
