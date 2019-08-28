4a1290f7553fa061a58633d4db14f0c4c7402f68
Denis Sidorov <dsigmat@hotmail.com>
2019-08-28 16:52:05 +0600

    end

--- ShopNetCore/Data/Models/Order.cs
+++ ShopNetCore/Data/Models/Order.cs
@@ -12,30 +12,30 @@ namespace ShopNetCore.Data.Models
         [BindNever]
         public int Id { get; set; }
         [Display(Name="Имя")]
-        [StringLength(5)]
+        [StringLength(25)]
         [Required(ErrorMessage ="Длина имени не менее 5 символов")]
         public string Name { get; set; }
 
         [Display(Name = "Фамилия")]
-        [StringLength(10)]
+        [StringLength(20)]
         [Required(ErrorMessage = "Длина Фамилия не менее 5 символов")]
         public string Surname { get; set; }
 
         [Display(Name = "Адрес")]
         [DataType(DataType.PostalCode)]
-        [StringLength(20)]
+        [StringLength(50)]
         [Required(ErrorMessage = "Длина Адрес не менее 5 символов")]
         public string Adress { get; set; }
 
         [Display(Name = "Номер телефона")]
         [DataType(DataType.PhoneNumber)]
-        [StringLength(10)]
+        [StringLength(20)]
         [Required(ErrorMessage = "Длина Номер телефона не менее 10 символов")]
         public string Phone { get; set; }
 
         [Display(Name = "Email")]
         [DataType(DataType.EmailAddress)]
-        [StringLength(15)]
+        [StringLength(25)]
         [Required(ErrorMessage = "Длина Email не менее 15 символов")]
         public string Email { get; set; }
 
