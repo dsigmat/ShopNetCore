3e2e4bcb480c26ab4c73e6c272a46773585d15bd
Denis Sidorov <dsigmat@hotmail.com>
2019-08-28 11:34:14 +0600

    Главная страница сайта

---
+++ ShopNetCore/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopNetCore.Data.Interfaces;
+using ShopNetCore.Data.Models;
+using ShopNetCore.ViewModels;
+
+namespace ShopNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars _cartRep;
+
+
+        public HomeController(IAllCars cartRep)
+        {
+            _cartRep = cartRep;
+        }
+        public IActionResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                favCars = _cartRep.GetFavCars
+            };
+            return View(homeCars);
+        }
+    }
+}
